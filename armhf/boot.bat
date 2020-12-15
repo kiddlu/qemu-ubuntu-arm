5f2cb699741bc17830bd5d4c7fac7700e84e6fc0
kiddlu <kidd.dawny.lu@gmail.com>
2020-12-15 18:54:31 +0800

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- armhf/boot.bat
+++ armhf/boot.bat
@@ -13,7 +13,7 @@ start "" "C:\Program Files\qemu\qemu-system-arm.exe" ^
   -M virt ^
   -device virtio-blk-device,drive=image ^
   -drive if=none,id=image,file=ubuntu-16.04-server-cloudimg-armhf-disk1.img ^
-  -netdev user,id=user0,hostfwd=tcp:127.0.0.1:50022-:22 ^
+  -netdev user,id=user0,hostfwd=tcp::50022-:22,hostfwd=tcp::50555-:5555,hostfwd=tcp::55037-:5037 ^
   -device e1000,netdev=user0
 
 :: wget http://ports.ubuntu.com/ubuntu-ports/dists/xenial/main/installer-armhf/current/images/generic-lpae/netboot/initrd.gz
