6232e8fc3d879a9a44ae425ab12e4bc6ad536288
kiddlu <kidd.dawny.lu@gmail.com>
2024-12-17 15:44:15 +0800

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- arm64/recovery.bat
+++ arm64/recovery.bat
@@ -4,8 +4,11 @@
 :: wget https://releases.linaro.org/components/kernel/uefi-linaro/latest/release/qemu64/QEMU_EFI.fd
 
 "C:\Program Files\qemu\qemu-system-aarch64.exe" ^
-    -smp 4 -m 4096 -M virt -bios QEMU_EFI.fd ^
+    -kernel vmlinuz ^
+    -initrd initrd.img ^
+    -append "root=/dev/vda1 ro recovery nomodeset" ^
+    -smp 4 -m 1024 -M virt -bios QEMU_EFI.fd ^
     -device virtio-blk-device,drive=image ^
     -drive if=none,id=image,file=noble-server-cloudimg-arm64.img ^
-    -netdev user,id=user0,hostfwd=tcp::2222-:22,hostfwd=tcp::2323-:23 -device virtio-net-device,netdev=user0 ^
+    -netdev user,id=user0 -device virtio-net-device,netdev=user0 -net user,hostfwd=tcp::2222-:22 ^
     -cpu cortex-a72
